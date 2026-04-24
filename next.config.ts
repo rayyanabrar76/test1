e2e5be2e704dd65ab9e01676018f6e3c935633d2
Rayyan Abrar <rayyanabrar76@gmail.com>
2026-04-24 23:30:43 +0500

    fix: remove pwa package

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 import type { NextConfig } from "next";
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-})
 
 const nextConfig: NextConfig = {
   typescript: {
@@ -31,4 +26,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
